c43d980b33601d786e00f84061a66deccf8264a4
Simen H Bakke <simenhbakke@gmail.com>
2017-11-08 08:02:43 +0100

    test_general done

--- PythonWrapper.cpp
+++ PythonWrapper.cpp
@@ -5,7 +5,9 @@
 #include <boost/python.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include "EncryptedArray.h"
-
+#include <cassert>
+#include <cstdio>
+#include <NTL/BasicThreadPool.h>
 //#include <boost/python/long.hpp>
 //#include <boost/python/docstring_options.hpp>
 #include <Python.h>
@@ -45,6 +47,13 @@ void debugCompare(EncryptedArray& ea, FHESecKey& sk, NewPlaintextArray& p, Ctxt&
 
 }
 
+void add_ctxt(Ctxt& a, Ctxt& b){
+      a += b;
+}
+
+void sub_ctxt(Ctxt& a, Ctxt& b){
+      a -= b;
+}
 
 BOOST_PYTHON_MODULE(PythonWrapper)
 {
@@ -59,6 +68,9 @@ BOOST_PYTHON_MODULE(PythonWrapper)
   def("CheckCtxt", CheckCtxt);
   def("debugCompare", debugCompare);
   def("makeIrredPoly", makeIrredPoly);
+  def("add_ctxt", add_ctxt);
+  def("sub_ctxt", sub_ctxt);
+
 
 
   void (*r1)(const EncryptedArray&, NewPlaintextArray&) = &random;
@@ -70,8 +82,17 @@ BOOST_PYTHON_MODULE(PythonWrapper)
   void (*a1)(const EncryptedArray&, NewPlaintextArray&, const NewPlaintextArray&) = &add;
   def("add", a1);
 
+  void (*s1)(const EncryptedArray&, NewPlaintextArray&, const NewPlaintextArray&) = &sub;
+  def("sub", s1);
+
   bool (*eq1)(const EncryptedArray&, const NewPlaintextArray&, const NewPlaintextArray&) = &equals;
   def("equals", eq1);
+
+  void (*sh1)(const EncryptedArray&, NewPlaintextArray&, long) = &shift;
+  def("shift", sh1);
+
+  void (*rt1)(const EncryptedArray&, NewPlaintextArray&, long) = &rotate;
+  def("rotate", rt1);
 
   //NTL functions
   long (*rb)(long) = &NTL::RandomBnd;
@@ -125,6 +146,7 @@ BOOST_PYTHON_MODULE(PythonWrapper)
 
   class_<Ctxt>("Ctxt", no_init)
     .def(init<const FHEPubKey&, long>())
+    .def(init<const Ctxt&>())
     .def("multiplyBy", &Ctxt::multiplyBy)
     .def("multByConstant", mbc1)
     .def("addConstant", addc1)
@@ -136,8 +158,9 @@ BOOST_PYTHON_MODULE(PythonWrapper)
   void (EncryptedArray::*e2)(Ctxt&, const FHEPubKey&, const NewPlaintextArray&) const = &EncryptedArray::encrypt;
   void (EncryptedArray::*d1)(const Ctxt&, const FHESecKey&, pyvector&) const = &EncryptedArray::decrypt;
   void (EncryptedArray::*d2)(const Ctxt&, const FHESecKey&, NewPlaintextArray&) const = &EncryptedArray::decrypt;
-
   void (EncryptedArray::*enc1)(ZZX&, const NewPlaintextArray&) const = &EncryptedArray::encode;
+  void (EncryptedArray::*eashift)(Ctxt&, long) const = &EncryptedArray::shift;
+  void (EncryptedArray::*earotate)(Ctxt&, long) const = &EncryptedArray::rotate;
 
   class_<EncryptedArray>("EncryptedArray", no_init)
     .def(init<const FHEcontext&, const ZZX&>())
@@ -146,11 +169,14 @@ BOOST_PYTHON_MODULE(PythonWrapper)
     .def("decrypt", d1)
     .def("decrypt_plaintext", d2)
     .def("encode", enc1)
+    .def("eashift", eashift)
+    .def("earotate", earotate)
     .def("size", &EncryptedArray::size)
   ;
 
   class_<NewPlaintextArray>("NewPlaintextArray", no_init)
     .def(init<const EncryptedArray&>())
+    .def(init<const NewPlaintextArray&>())
   ;
 
 
